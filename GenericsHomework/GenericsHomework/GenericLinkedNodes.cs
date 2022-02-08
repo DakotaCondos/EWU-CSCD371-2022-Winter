a1103930e613bd86dfb0ac417f4305e2d617a3eb
DakotaCondos <dakota242424@live.com>
2022-02-08 12:31:36 -0800

    update

---
+++ GenericsHomework/GenericsHomework/GenericLinkedNodes.cs
@@ -0,0 +1,37 @@
+﻿namespace GenericsHomework;
+
+public class GenericLinkedNodes<TValue>
+{
+    Node<TValue>? cursor;
+
+    GenericLinkedNodes()
+    {
+        cursor = null;
+    }
+
+    public void Append(TValue value)
+    {
+        Node<TValue> newNode = new(value);
+        //starting at cursor, walk to end (until node.next is cursor again,) then add
+        if (cursor is null)
+        {
+            cursor = newNode;
+        }
+        else
+        {
+            Node<TValue> lastInLine = GenericLinkedNodes<TValue>.Traverse(cursor);
+            lastInLine.Next = newNode;
+        }
+    }
+
+    private static Node<TValue> Traverse(Node<TValue> cursor)
+    {
+        Node<TValue> curentNode = cursor;
+        while (curentNode.Next != cursor)
+        {
+            curentNode = curentNode.Next;
+        }
+        return curentNode;
+    }
+}
+
--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,27 +1,26 @@
-﻿namespace GenericsHomework
+﻿namespace GenericsHomework;
+
+public class Node<TValue>
 {
-    public class Node<TValue>
+    public TValue Value { get; set; }
+    public Node<TValue> Next { get; set; } //check on the private set
+
+    public Node(TValue value)
     {
-        public TValue Value { get; set; }
-        public Node<TValue> Next { get; private set; } //check on the private set
-        
-        public Node(TValue value)
-        {
-            Value = value;
-            Next = this;
-        }
-        public void Append(TValue value)
-        {
-            Node<TValue> newNode = Next;
-        }
-        public void Clear()
-        {
-            Next = this;
-        }
-        
-        public override string ToString()
-        {
-            return Value.ToString();
-        }
+        Value = value;
+        Next = this;
+    }
+    
+    public void Clear()
+    {
+        Next = this;
+    }
+
+    public override string ToString()
+    {
+        return Value.ToString();
     }
 }
+
+
+//use cursor to hold a node as reference
